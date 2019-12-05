ac12d0cca3588253609d967c33799493bee4ffe4
Azamat Orozbaev <44497623+songhee24@users.noreply.github.com>
2019-12-05 19:08:11 +0600

    homework33

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+ 	// write your code here
+        String path = ".\\src\\com\\company\\Main.java";
+        FileWriter fileWriter = new FileWriter("file.txt");
+        FileWriter fileWriter1 = new FileWriter("file1.txt");
+        FileReader fileReader = new FileReader(path);
+        Scanner scanner = new Scanner(fileReader);
+        while (scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            fileWriter.write(line + "\n");
+            fileWriter1.write(line.trim());
+        }
+        fileReader.close();
+        fileWriter1.close();
+        fileWriter.close();
+        File file = new File("C:\\Users\\user\\IdeaProjects\\Homeworks\\file.txt");
+        try {
+            if (file.length() > 100.0){
+                throw new IllegalStateException();
+            } else throw new FileNotFoundException();
+        } catch (IllegalStateException ie){
+            System.out.println("код слишком большой");
+        } catch (FileNotFoundException fe){
+            System.out.println("файл не доступен");
+        }
+
+
     }
 }
