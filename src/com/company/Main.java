7cb7ab0b7adb4a51c80181d31f965e46cd7f6bbf
Azamat Orozbaev <44497623+songhee24@users.noreply.github.com>
2019-12-14 18:59:25 +0600

    homework37

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,63 @@
 package com.company;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] names = {
+                "oneDalmatian", "twoDalmatian","threeDalmatian","fourDalmatian", "fiveDalmatian","sixDalmatian",
+                "sevenDalmatian", "eightDalmatian","nineDalmatian","tenDalmatian", "elevenDalmatian","twelveDalmatian",
+                "thirteenDalmatian", "fourteenDalmatian","fifteenDalmatian","sixteenDalmatian", "seventeenDalmatian","eighteenDalmatian","nineteenDalmatian"
+        };
+
+        Random random = new Random();
+        Set<Dog>set = new HashSet<>();
+        for (int i = 0; i < 40;i++){
+            set.add(new Dog(i,names[random.nextInt(19)]));
+
+        }
+        System.out.println(set.size());
+
+        for (Dog d:set){
+            d.tellAboutYourSelf(set);
+        }
+
+
+
+
+
+
+    }
+
+
+}
+class Dog {
+    private int age;
+    private String name;
+
+
+    public Dog(int age,String name) {
+        this.age = age;
+        this.name = name;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Dog tellAboutYourSelf(Set<Dog> dog){
+        for (Dog d: dog){
+            System.out.println(d.age +" " + d.name);
+        }
+        return null;
     }
 }
