1c920078d9216cc7112c2aaaa1b2f26c385e88b4
Azamat Orozbaev <44497623+songhee24@users.noreply.github.com>
2020-02-18 13:34:45 +0600

    upd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,47 @@
 package com.company;
 
+import java.util.TreeSet;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        TreeSet<City> set = new TreeSet<>();
+        set.add(new City(1,"mon"));
+        set.add(new City(2,"tu"));
+        set.add(new City(3,"wed"));
+        set.add(new City(4,"thu"));
+        set.add(new City(5,"fri"));
+        set.add(new City(6,"sat"));
+        set.add(new City(7,"sun"));
+        System.out.println(set);
+
+    }
+
+
+}
+class City implements  Comparable{
+    int id;
+    String day;
+
+    public City(int id, String day) {
+        this.id = id;
+        this.day = day;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (!(o instanceof City)) return 0;
+        City temp = (City) o;
+        if (this.id > temp.id) return 1;
+        else if( this.id < temp.id) return -1;
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "DayOfWeek{" +
+                "day='" + day + '\'' +
+                '}';
     }
 }
